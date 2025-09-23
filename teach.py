fe85b90ff66ebfb1e84bc4ad6934e4e791b10e7f
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2025-09-23 17:52:38 +0000

    new array example

--- teach.py
+++ teach.py
@@ -1,9 +1,14 @@
 # python code
-import array as arr
-
-a = arr.array(
-    'h', # opcode for unsigned short
-    26
-)
+# import array
+from array import array
 
 print("hello world")
+
+a = array(
+    'h', # opcode for unsigned short
+    [26] # list
+)
+
+b = array('l', [1, 2, 3, 4, 5])
+
+print(b.pop())
