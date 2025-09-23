c5e78212395e2b3d88941c983f72882de3fda61a
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2025-09-23 17:55:53 +0000

    merged

--- teach.py
+++ teach.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # python code
 # import array
 from array import array
@@ -13,16 +12,3 @@ a = array(
 b = array('l', [1, 2, 3, 4, 5])
 
 print(b.pop())
-=======
-# python hello world
-
-hello = "world"
-
-this_number = "42"
-
-this_number = int(this_number) + 5
-
-this_number + 1
-
-print(this_number, 5)
->>>>>>> 25fb6eea255a05f093aa3084bc20394cca7f1cb3
