d9f99f11b74824d38c602ac210d6823b607e8498
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2026-08-27 22:57:01 +0000

    m

---
+++ helloWorld.py
@@ -0,0 +1,14 @@
+import numpy
+
+happyFace = [1,2,3,4]
+smily = numpy.array(happyFace)
+
+print(smily)
+
+happyChar = ['a','b','c','d']
+happyChar1 = ["words", "to", "speak"]
+happyBools = [True, False, True]
+happyBools2 = [[1,2,3], [True, False, True], ["words"]]
+
+print("hello world")
+print(happyBools2)
